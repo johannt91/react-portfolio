48986ce228d2304788b8a2e72a3442c28a970f43
Johann Taylor <j.taylor1343@gmail.com>
2022-01-01 13:34:20 -0500

    Grammar fix in about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,7 +15,7 @@ function About() {
             </div>
             <div className="about-content">
                 <p>I am a pianist and piano teacher who has recently transitioned into the world of tech. 
-                    As a performer and teacher I constantly push my capacity for learning and improving in order to be on top of my game. 
+                    As a teacher and performer, I constantly push my capacity for improvement and learning in order to be on top of my game. 
                     Pushing my capacity along with my passion and dedication have all contributed to the successes on my coding journey.
                 </p>
                 <p>
@@ -23,12 +23,7 @@ function About() {
                 It has enabled me to foster my critical thinking skills, attention to detail, as well as breaking down of complex ideas into simpler components. 
                 In this rapidly evolving technological landscape, my thirst for knowledge and curiosity fuel my desire to keep up with the latest technologies.
                 </p>
-                <p>
-
-                </p>
             </div>
-  
-
         </section>
     );
 }
